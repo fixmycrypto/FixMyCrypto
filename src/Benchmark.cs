d50cfddf2426e82e59aceb9ba909a20640a9d0e4
Fix My Crypto <help@fixmycrypto.com>
2021-11-16 16:06:18 -0500

    WIP benchmarking

--- src/Benchmark.cs
+++ src/Benchmark.cs
@@ -33,18 +33,28 @@ namespace FixMyCrypto {
 
         [Benchmark]
         public void PassphraseGuess() {
-            string passphrase = "((C||c)orrect&&(H||h)orse&&(B||b)attery&&(S||s)taple)[1-9]?[0-9][^a-zA-Z0-9]";
+            // string passphrase = "((C||c)orrect&&(H||h)orse&&(B||b)attery&&(S||s)taple)[1-9]?[0-9][^a-zA-Z0-9]";
+            string passphrase = "(The||the)(P||p)assphrase[0-9]?[!@#$%^&*()]?";
             Passphrase p = new Passphrase(passphrase);
             int count = 0;
-            foreach (string r in p.Enumerate()) count++;
+            Parallel.ForEach(p.Enumerate(), r => {
+                byte[] salt = Encoding.UTF8.GetBytes("mnemonic" + r);
+                var seed = Cryptography.Pbkdf2_HMAC512("siren bottom inform vehicle else donkey dirt task cook tide general when", salt, 2048, 64);
+                count++;
+            }); 
         }
 
         [Benchmark]
         public void PassphraseFuzz() {
             string passphrase = "{{ThePassphrase!}}";
-            Passphrase p = new Passphrase(passphrase, fuzzDepth: 2);
+            // Passphrase p = new Passphrase(passphrase, fuzzDepth: 2);
+            Passphrase p = new Passphrase(passphrase, fuzzDepth: 1);
             int count = 0;
-            foreach (string r in p.Enumerate()) count++;
+            Parallel.ForEach(p.Enumerate(), r => {
+                byte[] salt = Encoding.UTF8.GetBytes("mnemonic" + r);
+                var seed = Cryptography.Pbkdf2_HMAC512("siren bottom inform vehicle else donkey dirt task cook tide general when", salt, 2048, 64);
+                count++;
+            }); 
         }
 
         public static BenchmarkDotNet.Reports.Summary RunBenchmarks() {
